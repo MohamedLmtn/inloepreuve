4e179b8ebb97f7998acacfe31ee90abde4c9846c
MohamedLmtn <mohamed.lmtn@eduge.ch>
2024-01-09 09:38:45 +0100

    test complete code complete

--- evalinlo/main.py
+++ evalinlo/main.py
@@ -1,6 +1,6 @@
 class vente_enchere:
         
-    def ecnchere(self,nomprod : str,prixprod: int,prixenchere : int,diffprct : int = 100) -> bool:
+    def enchere(self,nomprod : str,prixprod: int,prixenchere : int,diffprct : int = 100) -> bool:
         if (prixenchere % 10) == 0 and prixenchere > prixprod and (prixenchere*100) / prixprod > 120:
             if (((prixenchere *100)/prixprod) - 100) >= diffprct:
                 return True
@@ -8,4 +8,15 @@ class vente_enchere:
                 return False
         else :
             return False
+        
+
+    def offres(self,name : str = None,prix : int = None):
+        lstoffre = {"bague or":100,"bracelet":120}
+
+        if name in lstoffre.keys() or prix in lstoffre.values():
+            return True
+        else :
+            return False
+        
     
+
--- evalinlo/test.py
+++ evalinlo/test.py
@@ -5,28 +5,43 @@ import unittest
 class unitest(unittest.TestCase):
     
     def test1(self):
-        self.assertEqual(True,vente_enchere.ecnchere(self,'bague',50,100))
+        self.assertEqual(True,vente_enchere.enchere(self,'bague',50,100))
         
     def test2(self):
-        self.assertEqual(False,vente_enchere.ecnchere(self,'bague',40,12))
+        self.assertEqual(False,vente_enchere.enchere(self,'bague',40,12))
         
     def test3(self):
-        self.assertEqual(False,vente_enchere.ecnchere(self,'bague',50,50))
+        self.assertEqual(False,vente_enchere.enchere(self,'bague',50,50))
     
     def test4(self):
-        self.assertEqual(True,vente_enchere.ecnchere(self,'bague',50,150))
+        self.assertEqual(True,vente_enchere.enchere(self,'bague',50,150))
     
     def test5(self):
-        self.assertEqual(False,vente_enchere.ecnchere(self,'bague',50,50))
+        self.assertEqual(False,vente_enchere.enchere(self,'bague',50,50))
         
     def test6(self):
-        self.assertEqual(False,vente_enchere.ecnchere(self,'bague',50,60))
+        self.assertEqual(False,vente_enchere.enchere(self,'bague',50,60))
         
     def test7(self):
-        self.assertEqual(True,vente_enchere.ecnchere(self,'bague',50,150,200))
+        self.assertEqual(True,vente_enchere.enchere(self,'bague',50,150,200))
         
     def test8(self):
-        self.assertEqual(False,vente_enchere.ecnchere(self,'bague',50,100,200))
+        self.assertEqual(False,vente_enchere.enchere(self,'bague',50,100,200))
+
+    def test9(self):
+        self.assertEqual(True,vente_enchere.offres(self,name="bague or"))
+
+    def test10(self):
+        self.assertEqual(True,vente_enchere.offres(self,prix=100))
+    
+    def test11(self):
+        self.assertEqual(True,vente_enchere.offres(self,"bracelet",120))
+
+    
+    def test12(self):
+        self.assertEqual(False,vente_enchere.offres("test",666))
+
+    
                 
 if __name__ == '__main__':
     unittest.main()
